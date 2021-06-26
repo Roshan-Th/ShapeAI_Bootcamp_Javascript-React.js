05bb6475710b527f98dd0d87126aa64fa7bc1dfc
Roshan-Th <76696412+Roshan-Th@users.noreply.github.com>
2021-06-26 18:30:11 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Information from "./Information";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Information />
+      <Information />
+      <Information />
+      <Information />
+      <Information />
+      <Footer />
+    </div>
+  );
+}
+export default App;
---
+++ Information.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Information() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        An amazing bootcamp taken by Shaurya Sinha. Covered everything from
+        Scratch including Javascript, React.js,HTML.
+      </p>
+    </div>
+  );
+}
+export default Information;
